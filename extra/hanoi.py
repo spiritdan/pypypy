42c5fb7dbb952070ad0d9928f56e6d94cd09c5b1
spiritdan <37323553+spiritdan@users.noreply.github.com>
2018-12-26 17:25:23 +0800

    汉诺塔

---
+++ extra/hanoi.py
@@ -0,0 +1,9 @@
+def move(n, a, b, c):
+    if n==1:
+        print(a,'-->',c)
+        return
+    else:
+        move(n-1,a,c,b)  #首先需要把 (N-1) 个圆盘移动到 b
+        move(1,a,b,c)    #将a的最后一个圆盘移动到c
+        move(n-1,b,a,c)  #再将b的(N-1)个圆盘移动到c
+move(4, 'A', 'B', 'C')
